870b7c4f0e499b6bfb8053397a9c0d3e6ed1c01c
Olimpio Almeida Sakamoto <oasakamoto@proton.me>
2024-09-30 18:14:44 -0300

    feat: configura prisma com o nestjs

---
+++ src/common/prisma/prisma.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import {PrismaClient as PrismaClientPostgres} from '../../../prisma/generate/postgres';
+
+@Injectable()
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
+  private postgresClient: PrismaClientPostgres;
+
+  constructor() {
+    this.postgresClient = new PrismaClientPostgres();
+  }
+
+  async onModuleInit() {
+      await this.postgresClient.$connect();
+  }
+
+  async onModuleDestroy() {
+      await this.postgresClient.$disconnect();
+  }
+
+  get postgres() {
+    return this.postgresClient;
+  }
+}
--- src/signup/signup.module.ts
+++ src/signup/signup.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { SignUpController } from "./signup.controller";
+import { PrismaModule } from "src/common/prisma/prisma.module";
 
 @Module({
+  imports: [PrismaModule],
   controllers: [SignUpController],
 })
 
